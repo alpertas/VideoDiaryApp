2b42bcf234cdff46897978f6c455f5cb7703d58b
alpertas <alpertas.cpp@gmail.com>
2025-11-18 16:24:31 +0300

    chore(deps): update babel configuration and upgrade react-native-reanimated and safe-area-context versions

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
     ],
-    plugins: ["react-native-reanimated/plugin"],
   };
 };
 
